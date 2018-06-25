8c36562b8084fffd6a259ad537316a2b380f7cc0
David Darmon <gendarmon@gmail.com>
2018-06-25 10:27:21 -0400

    Adding a testing of embedding using multiple lags.

---
+++ sidpy/load_models.py
@@ -0,0 +1,373 @@
+import random
+import math
+import numpy
+import matplotlib.pyplot as plt
+import scipy.special
+import os
+import pandas
+
+import sdeint
+
+def load_model_data(model_name, N):
+	if model_name == 'arch':
+		# Simulate from an ARCH(1) model:
+		model_type = 'arch'
+		p_true = 1
+		x = numpy.zeros(N)
+		u = numpy.random.randn(N)
+
+		x[:1] = u[:1]
+
+		for n in range(1, x.shape[0]):
+			x[n] = numpy.sqrt(0.5 + 0.5*x[n-1]**2)*u[n]
+
+		# Simulate from an ARCH(2) model:
+		# p_true = 2
+
+		# x = numpy.zeros(N)
+		# u = numpy.random.randn(N)
+
+		# x[:2] = u[:2]
+
+		# for n in range(2, x.shape[0]):
+		# 	x[n] = numpy.sqrt(0.5 + 0.5*x[n-1]**2 + 0.5*x[n-2]**2)*u[n]
+
+	if model_name == 'slogistic' or model_name == 'logistic':
+		# NLAR(1) model from:
+		# 
+		# **Estimation of conditional densities and sensitivity measures in nonlinear dynamical systems**
+		model_type = 'nlar'
+		p_true = 1
+		x = numpy.zeros(N)
+
+		if model_name == 'slogistic':
+			noise = (numpy.random.rand(N, 48) - 0.5)/2./16.
+			noise = noise.sum(1)
+		else:
+			noise = numpy.zeros(N)
+
+		x[0] = numpy.random.rand(1)
+		for t in range(1, x.shape[0]):
+			x[t] = 3.68*x[t-1]*(1 - x[t-1]) + 0.4*noise[t]
+
+			if x[t] <= 0 or x[t] >= 1:
+				x[t] = numpy.random.rand(1)
+
+	if model_name == 'shenon' or model_name == 'henon':
+		# NLAR(2) model from:
+		# 
+		# **On prediction and chaos in stochastic systems**
+		#
+		# Namely, stochastic Henon.
+		#
+		# Also on page 188 (dp 200) of *Chaos: A Statistical Perpsective*.
+		model_type = 'nlar'
+		p_true = 2
+
+		burnin = 1000
+
+		x = numpy.zeros(N + burnin)
+
+		if model_name == 'shenon':
+			# From original paper:
+			noise = (numpy.random.rand(N + burnin, 48) - 0.5)/2.
+			noise = noise.sum(1)
+		else:
+			noise = numpy.zeros(N + burnin)
+
+		x[:2] = 10*numpy.random.rand(1) - 5
+		for t in range(2, x.shape[0]):
+			x[t] = 6.8 - 0.19*x[t-1]**2 + 0.28*x[t-2] + 0.2*noise[t]
+
+			if x[t] < -10:
+				x[t] = 10*numpy.random.rand(1) - 5
+
+		x_w_burnin = x.copy()
+		x = x[burnin:]
+
+	if model_name == 'stent' or model_name == 'tent':
+		# Tent map with noise as from the logistic map.
+		# This is also a SETAR(1; 1, 1) model, in the
+		# vein of Tong.
+
+		model_type = 'nlar'
+		p_true = 1
+
+		x = numpy.zeros(N)
+
+		if model_name == 'stent':
+			noise = (numpy.random.rand(N, 48) - 0.5)/2./16.
+			noise = noise.sum(1)
+		else:
+			noise = numpy.zeros(N)
+
+		# No noise:
+		# noise = numpy.zeros(N)
+
+		x[0] = numpy.random.rand(1)
+		for t in range(1, x.shape[0]):
+		    if x[t-1] < 0.5:
+		        x[t] = 3.68/2.*x[t-1] + 0.4*noise[t]
+		    else:
+		        x[t] = 3.68/2. - 3.68/2.*x[t-1] + 0.4*noise[t]
+
+		    if x[t] <= 0:
+		       x[t] = numpy.random.rand(1)
+
+	if model_name == 'setar':
+		model_type = 'setar'
+
+		# Simulate from a SETAR(2; 1, 1) model:
+
+		# p_true = 1
+
+		# x = numpy.zeros(N)
+		# u = numpy.random.randn(N)
+
+		# x[:1] = u[:1]
+
+		# for n in range(1, x.shape[0]):
+		# 	# From "Sieve bootstrap for time series" by Buhlmann in Bernoulli
+		# 	if x[n-1] <= 0:
+		# 		x[n] = 1.5 - 0.9*x[n-1] + u[n]
+		# 	else:
+		# 		x[n] = -0.4 - 0.6*x[n-1] + u[n]
+
+			# if x[n-1] <= 0:
+			# 	x[n] = 1.5 - 0.9*x[n-1] + 0.5*u[n]
+			# else:
+			# 	x[n] = -0.4 - 0.6*x[n-1] + u[n]
+
+			# if x[n-1] <= 0:
+			# 	x[n] = 1.5 - 0.9*x[n-1] + 0.5*u[n]
+			# else:
+			# 	x[n] = -0.4 + 0.1*x[n-1] + u[n]
+
+		# Simulate from a SETAR(2; 2, 2) model:
+		# p_true = 2
+		# x = numpy.zeros(N)
+		# u = numpy.random.randn(N)
+
+		# x[:2] = u[:2]
+
+		# for n in range(2, x.shape[0]):
+		# 	if x[n-1] <= 0:
+		# 		x[n] = 1.5 - 0.9*x[n-1] + 0.2*x[n-2] + u[n]
+		# 	else:
+		# 		x[n] = -0.4 - 0.6*x[n-1] + 0.1*x[n-2] + u[n]
+
+		# Simulate from a SETAR(2; 2, 2) model used to model 
+		# the Canadian Lynx data from Tong's *Non-linear 
+		# Time Series*, p. 178:
+		p_true = 2
+		x = numpy.zeros(N)
+		u = numpy.random.randn(N)
+
+		x[:2] = 2*numpy.random.rand(2) + 2
+
+		for n in range(2, x.shape[0]):
+		  if x[n-2] <= 3.25:
+		      x[n] = 0.62 + 1.25*x[n-1] - 0.43*x[n-2] + numpy.sqrt(0.0381)*u[n]
+		  else:
+		      x[n] = 2.25 + 1.52*x[n-1] - 1.24*x[n-2] + numpy.sqrt(0.0626)*u[n]
+
+	if 'lorenz' in model_name:
+		model_type = 'nlar'
+
+		p_true = 4
+
+		h = 0.05
+
+		ds_by = 2
+
+		ttot = N*h*ds_by
+		tburn = 20
+		tf = ttot + tburn
+
+		tspan = numpy.linspace(0.0, tf, int(tf/h))
+		x0 = numpy.array([1.0, 1.0, 1.0])
+
+		params = [10., 28., 8./3.] # The parameters [s, r, b] for the canonical Lorenz equations
+
+		def F(X, t):
+			s = params[0]
+			r = params[1]
+			b = params[2]
+
+			x = X[0]
+			y = X[1]
+			z = X[2]
+
+			dX = numpy.array([s*(y - x), x*(r - z) - y, x*y - b*z])
+
+			return dX
+
+		def G(x, t):
+		    return B
+
+		dim = 3
+
+		if model_name == 'slorenz':
+			dyn_noise = 2
+		else:
+			dyn_noise = 0
+
+		B = numpy.diag([dyn_noise]*dim)
+
+		result = sdeint.itoint(F, G, x0, tspan)
+
+		tspan = tspan[int(tburn/h)::ds_by]
+		result = result[int(tburn/h)::ds_by, :]
+
+		x = result[:, 0]
+
+	if 'rossler' in model_name:
+		model_type = 'nlar'
+
+		p_true = 4
+
+		h = 0.05
+
+		ds_by = 5
+
+		ttot = N*h*ds_by
+		tburn = 100
+		tf = ttot + tburn
+
+		tspan = numpy.linspace(0.0, tf, int(tf/h))
+		x0 = numpy.array([1.0, 1.0, 1.0])
+
+		params = [0.1, 0.1, 14]
+
+		def F(X, t):
+			a = params[0]
+			b = params[1]
+			c = params[2]
+
+			x = X[0]
+			y = X[1]
+			z = X[2]
+
+			dX = numpy.array([-y - z, x + a*y, b + z*(x - c)])
+
+			return dX
+
+		def G(x, t):
+		    return B
+
+		dim = 3
+
+		if model_name == 'srossler':
+			dyn_noise = 0.1
+		else:
+			dyn_noise = 0.0
+
+		B = numpy.diag([dyn_noise]*dim)
+		B[2, 2] = 0
+
+		result = sdeint.itoint(F, G, x0, tspan)
+
+		tspan = tspan[int(tburn/h)::ds_by]
+		result = result[int(tburn/h)::ds_by, :]
+
+		x = result[:, 0]
+		y = result[:, 1]
+		z = result[:, 2]
+
+	if model_name == 'snanopore':
+		a=1.
+		b=1.
+		c=1.
+		km=5.
+		kp=1.
+		gamx=1.
+		gamy=100.
+		alpha=0.1
+		beta=0.1
+
+		model_type = 'nlar'
+
+		p_true = numpy.inf
+
+		ds_by = 2
+
+		delta_t=0.25
+		T_final = ds_by*N*delta_t
+
+		M = int(T_final/delta_t + 1)
+
+		tspan = numpy.linspace(0.0, T_final, M)
+		x0 = numpy.array([1.0, -0.5])
+
+		def f(z, t):
+		#z=[x,y]
+			dx=-(1/gamx)*(a*numpy.power(z[0],3)-b*z[0]+c*z[1])
+			if z[0]<0:
+				H=0
+			else:
+				H=1
+			if z[0]>0:
+				Hm=0
+			else:
+				Hm=1
+			dy=(1/gamy)*(kp*H-km*Hm)
+			return numpy.array([dx,dy]) 
+
+		def G(z, t):
+			B=numpy.diag([alpha,beta])
+			return B
+
+		print 'Solving SDE...'
+
+		result = sdeint.itoint(f, G, x0, tspan)
+
+		x = result[::ds_by, 0]
+
+	if model_name == 'shadow_crash':
+		p_true = 4
+		model_type = 'nlar'
+
+		h = 0.05
+
+		ds_by = 2
+
+		ttot = N*h*ds_by
+		tburn = 20
+		tf = ttot + tburn
+
+		tspan = numpy.linspace(0.0, tf, int(tf/h))
+		x0 = numpy.array([1.0, 1.0])
+
+		def F(X, t):
+			b = 0.42
+			g = -0.04
+			
+			x = X[0]
+			z = X[1]
+			
+			dX = numpy.array([x - x**2*numpy.exp(-b*x*z), z - z**2*numpy.exp(-g*x)])
+
+			return dX
+
+		def G(X, t):
+			# B = numpy.diag([0.4, 0.01])
+			B = numpy.diag([0.2, 0.01])
+
+			x = X[0]
+			z = X[1]
+
+			B[0, 0] = x*B[0, 0]
+			B[1, 1] = z*B[1, 1]
+			
+			return B
+
+		dim = 2
+
+		result = sdeint.itoint(F, G, x0, tspan)
+
+		tspan = tspan[int(tburn/h)::ds_by]
+		result = result[int(tburn/h)::ds_by, :]
+
+		x = result[:, 0]
+
+	return x, p_true, model_type
--- tests/test_embed_multilag.py
+++ tests/test_embed_multilag.py
@@ -7,10 +7,137 @@ import sys
 sys.path.append('/Users/{}/Documents/Reference/G/github/sidpy/sidpy'.format(username))
 
 import sidpy
+import load_models
 
 import numpy
 
 import matplotlib.pyplot as plt
+
+from sklearn import neighbors
+from sklearn.metrics import pairwise
+
+import pyflann
+import nlopt
+
+import scipy
+
+def loocv_mse(n_neighbors, neighbor_inds, X):
+	"""
+	loocv_mse computes the mean-squared error
+	of a k-nearest predictor using 
+	leave-one-crossvalidation, where X is 
+	the regression matrix where the first
+	p columns correspond to the p past values
+	and the (p+1) column corresponds to the future.
+
+	loocv_mse is set up to run with nlopt.
+
+	Parameters
+	----------
+	n_neighbors : float
+			The current candidate number of neighbors to use
+			for the k-nearest neighbor predictor.
+
+	neighbor_inds : numpy.array
+			The array of the nearest neighbors for an
+			evaluation point. Each **row** corresponds
+			to an evaluation point, and each **column**
+			corresponds to the kth-nearest neighbor.
+
+	X : numpy.array
+			The regression matrix. The future, which is
+			used for both prediction and evaluation, is
+			the right-most column.
+
+	Returns
+	-------
+	mse : float
+			The leave-one-out cross-validated mean-squared
+			error of the k-nearest neighbor predictor.
+	"""
+
+	n_neighbors = int(numpy.floor(n_neighbors))
+
+	err = numpy.power(X[:, -1] - numpy.mean(X[neighbor_inds[:, :n_neighbors], -1], 1), 2)
+	mse = numpy.mean(err)
+	
+	# print s, n_neighbors, mse
+
+	return mse
+
+def choose_lag_mse(X, pow_upperbound = 0.5, nn_package = 'sklearn', announce_stages = False, output_verbose = True):
+	Lp_norm = 2.
+
+	p_max = X.shape[1] - 1
+
+	X_full = X.copy()
+
+	mse_by_p = []
+	kstar_by_p = []
+
+	ps = range(2, p_max + 1)
+
+	for p_use in ps:
+		X = X_full[:, (p_max - p_use):]
+
+		n_neighbors = int(numpy.ceil(numpy.power(X.shape[0] - 1, pow_upperbound)))
+
+		n_neighbors_upperbound = n_neighbors
+
+		if announce_stages:
+			print('Computing nearest neighbor distances using k_upper = {}...'.format(n_neighbors))
+
+		# Compute the nearest neighbor distances and nearest neighbor indices
+		# in the marginal space.
+
+		Z = X[:, :p_use]
+
+		if nn_package == 'pyflann':
+			flann = pyflann.FLANN()
+
+			neighbor_inds, distances_marg = flann.nn(Z,Z,n_neighbors + 1);
+
+			neighbor_inds = neighbor_inds[:, 1:]
+			distances_marg = distances_marg[:, 1:]
+
+			distances_marg = numpy.sqrt(distances_marg) # Since FLANN returns the *squared* Euclidean distance.
+		elif nn_package == 'sklearn':
+			knn = neighbors.NearestNeighbors(n_neighbors, algorithm = 'kd_tree', p = Lp_norm)
+
+			knn_out = knn.fit(Z)
+
+			distances_marg, neighbor_inds = knn_out.kneighbors()
+		else:
+			assert False, "Please select either 'sklearn' or 'pyflann' for nn_package."
+
+		if announce_stages:
+			print('Done computing nearest neighbor distances...')
+
+		if announce_stages:
+			print('Tuning nearest neighbor number...')
+
+		opt_out = scipy.optimize.minimize_scalar(loocv_mse, bounds = [1.0, n_neighbors_upperbound], method = 'bounded', args = (neighbor_inds, X))
+
+		if announce_stages:
+			print('Done tuning nearest neighbor number...')
+
+		k_opt = int(numpy.floor(opt_out['x']))
+
+		kstar_by_p += [k_opt]
+
+		if output_verbose:
+			print('For p = {}, chose k* = {} with MSE(k*) = {}'.format(p_use, k_opt, opt_out['fun']))
+
+		mse_by_p += [opt_out['fun']]
+
+		if n_neighbors_upperbound - k_opt <= 10:
+			print("####################################################\n# Warning: For p = {}, Nelder-Mead is choosing k* near k_upper = {}.\n# Increase pow_upperbound.\n####################################################""".format(p_use, n_neighbors_upperbound))
+
+	p_opt = ps[numpy.argmin(mse_by_p)]
+	mse_opt = numpy.min(mse_by_p)
+
+	return p_opt, mse_opt, mse_by_p, kstar_by_p
+
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
@@ -26,14 +153,12 @@ dm = 2
 
 x = numpy.arange(0, 4*(Tp + tf), dt)
 
-print("\nUsing dt = {}...".format(dt))
-
 X = sidpy.embed_ts_multilag(x, dt, Tp, tf)
 X_pf = sidpy.extract_multilag_from_embed(X, dt, Tp, tf, dm)
 
-print(X)
-print("\n")
-print(X_pf)
+# print(X)
+# print("\n")
+# print(X_pf)
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
@@ -48,15 +173,14 @@ tf = 5
 
 dm = 2
 
-
 x = numpy.arange(0, 4*(Tp + tf), dt)
 
 X = sidpy.embed_ts_multilag(x, dt, Tp, tf)
 X_pf = sidpy.extract_multilag_from_embed(X, dt, Tp, tf, dm)
 
-print(X)
-print("\n")
-print(X_pf)
+# print(X)
+# print("\n")
+# print(X_pf)
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #
@@ -65,11 +189,26 @@ print(X_pf)
 #
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-x = numpy.loadtxt('../example-data/data-snanopore/NRK5.txt')
+numpy.random.seed(1)
+
+N = 50000
+# N = 1000
+
+model_name = 'snanopore'
+# model_name = 'slorenz'
+# model_name = 'srossler'
+# model_name = 'shadow_crash'
+
+# model_name = 'slogistic'
+
+# model_name = 'lorenz'
+# model_name = 'rossler'
+
+x, p_true, model_type = load_models.load_model_data(model_name = model_name, N = N)
 
 dt = 1.0
 Tp = 10
-tf = 10
+tf = 100
 
 time = numpy.arange(0, len(x)*dt, dt)
 
@@ -84,5 +223,18 @@ for dm in [1, 2, 4, 5, 8, 10]:
 	TIME_pf = sidpy.extract_multilag_from_embed(TIME, dt, Tp, tf, dm)
 
 	plt.plot(TIME_pf[0, :], X_pf[0, :], '.')
+
+plt.show()
+
+for dm in [1, 2, 4, 5, 8, 10]:
+	print('\n\n')
+	print("Using dm = {}".format(dm))
+	X_pf = sidpy.extract_multilag_from_embed(X, dt, Tp, tf, dm)
+
+	# pow_upperbound = 0.5
+	pow_upperbound = 0.66
+	# pow_upperbound = 0.75
+
+	p_opt, mse_opt, mse_by_p, kstar_by_p = choose_lag_mse(X_pf, pow_upperbound = pow_upperbound, nn_package = 'sklearn', announce_stages = False, output_verbose = True)
 
 plt.show()
